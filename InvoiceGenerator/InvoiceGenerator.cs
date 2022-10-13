92ecb0e04f2cb6a8cfb6393ea8444e55fe5c48dd
sayalipagar <pagarsayali565@gmail.com>
2022-10-13 22:17:24 +0530

    [Sayali]Add:CalculateFare

---
+++ InvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+   
+        public class InvoiceGenerator
+        {
+            private static readonly double COST_PER_KILOMETER = 10.0;
+            private static readonly double COST_PER_MINUTE = 1.0;
+            private static readonly double MINIMUM_FARE = 5.0;
+            private double cabFare = 0.0;
+
+            //UC1 - Calculate Fare 
+            public double CalculatFare(double distance, double time)
+            {
+                this.cabFare = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+                return Math.Max(this.cabFare, MINIMUM_FARE);
+            }
+        }
+    }
+       
+
---
+++ InvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();  
+            double fare = invoiceGenerator.CalculatFare(2.0, 4.0);
+            Console.WriteLine("The Total Fare For The Journey : " + fare);
+        }
+    }
+}
